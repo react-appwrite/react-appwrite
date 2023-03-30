b3e489d68edb3e48466278bcde2351c8bfdea8d6
dishwasher-detergent <kenny_bass@outlook.com>
2023-03-29 19:32:06 -0500

    Add anonymous sign ins

---
+++ src/account/useAnonSignIn.ts
@@ -0,0 +1,23 @@
+"use client";
+
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { Models } from "appwrite";
+import { useAppwrite } from "react-appwrite";
+
+function useAnonSignIn() {
+  const { account } = useAppwrite();
+  const queryClient = useQueryClient();
+  const mutation = useMutation<Models.Session>({
+    mutationFn: async (request) => {
+      return await account.createAnonymousSession();
+    },
+
+    onSuccess: async () => {
+      queryClient.setQueryData(["appwrite", "account"], await account.get());
+    },
+  });
+
+  return mutation;
+}
+
+export { useAnonSignIn };
