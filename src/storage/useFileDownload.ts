492b72770360777a0e3f17a1d752417af4d1c68c
sanny-io <sannysherief@gmail.com>
2023-04-01 03:48:28 -0700

    Convert useFileDownload to query.

--- src/storage/useFileDownload.ts
+++ src/storage/useFileDownload.ts
@@ -1,24 +1,32 @@
 'use client'
 
-import { useMutation } from '@tanstack/react-query'
+import { useQuery } from '@tanstack/react-query'
+import { useMemo } from 'react'
 import { useAppwrite } from 'react-appwrite'
-
-type Props = {
-  bucketId: string,
-  fileId: string,
-}
 
 /**
  * Retrieves a file URL for download.
+ * @param bucketId The bucket the file belongs to.
+ * @param fileId The unique ID of the file.
  * @link [Appwrite Documentation](https://appwrite.io/docs/client/storage?sdk=web-default#storageGetFileDownload)
  */
-export function useFileDownload() {
+export function useFileDownload(
+  bucketId?: string,
+  fileId?: string,
+) {
   const { storage } = useAppwrite()
-  const mutation = useMutation({
-    mutationFn: async ({ bucketId, fileId, }: Props) => {
-      return storage.getFileDownload(bucketId, fileId)
+  const queryKey = useMemo(() => ['appwrite', 'storage', 'downloads', bucketId, fileId], [bucketId, fileId])
+  const queryResult = useQuery<URL | null, unknown, URL | null, (string | void)[]>({
+    queryKey,
+    enabled: !!bucketId && !!fileId,
+    queryFn: ({ queryKey: [, , , bucketId, fileId] }) => {
+      if (bucketId && fileId) {
+        return storage.getFileDownload(bucketId, fileId)
+      }
+
+      return null
     },
   })
 
-  return mutation
+  return queryResult
 }
