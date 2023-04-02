c0ff00f39eca7ff2f4fe0b6e8b6834ac2fd26527
dishwasher-detergent <kenny_bass@outlook.com>
2023-04-02 07:33:52 -0500

    Move image types to seperate types file

--- src/storage/types.ts
+++ src/storage/types.ts
@@ -2,3 +2,25 @@ export type StorageFileOperation =
   | 'create'
   | 'update'
   | 'delete'
+
+export type Image = PreviewImage | PreviewBorder | PreviewDimensions
+
+export type PreviewImage = {
+  gravity?: number,
+  quality?: number,
+  opacity?: number,
+  rotation?: number,
+  background?: string,
+  output?: string,
+}
+
+export type PreviewBorder = {
+  borderColor?: string,
+  borderRadius?: number,
+  borderWidth?: number,
+}
+
+export type PreviewDimensions = {
+  width?: number,
+  height?: number
+}
