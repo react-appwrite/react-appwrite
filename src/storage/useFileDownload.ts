03e40a0a2e8e13c25e19109c743f62b64294067e
sanny-io <sannysherief@gmail.com>
2023-04-03 18:35:07 -0700

    Add options to useFileView and useFileDownload

--- src/storage/useFileDownload.ts
+++ src/storage/useFileDownload.ts
@@ -1,6 +1,6 @@
 'use client'
 
-import { useQuery } from '@tanstack/react-query'
+import { UseQueryOptions, useQuery } from '@tanstack/react-query'
 import { useMemo } from 'react'
 import { useAppwrite } from 'react-appwrite'
 
@@ -8,15 +8,17 @@ import { useAppwrite } from 'react-appwrite'
  * Retrieves a file URL for download.
  * @param bucketId The bucket the file belongs to.
  * @param fileId The unique ID of the file.
+ * @param options Options to pass to `react-query`.
  * @link [Appwrite Documentation](https://appwrite.io/docs/client/storage?sdk=web-default#storageGetFileDownload)
  */
 export function useFileDownload(
   bucketId?: string,
   fileId?: string,
+  options?: UseQueryOptions<URL | null, unknown, URL | null, (string | void)[]>
 ) {
   const { storage } = useAppwrite()
   const queryKey = useMemo(() => ['appwrite', 'storage', 'downloads', bucketId, fileId], [bucketId, fileId])
-  const queryResult = useQuery<URL | null, unknown, URL | null, (string | void)[]>({
+  const queryResult = useQuery({
     queryKey,
     enabled: !!bucketId && !!fileId,
     queryFn: ({ queryKey: [, , , bucketId, fileId] }) => {
@@ -26,6 +28,10 @@ export function useFileDownload(
 
       return null
     },
+
+    cacheTime: 0,
+
+    ...options,
   })
 
   return queryResult
--- src/storage/useFileView.ts
+++ src/storage/useFileView.ts
@@ -1,16 +1,20 @@
 'use client'
 
-import { useQuery } from '@tanstack/react-query'
+import { UseQueryOptions, useQuery } from '@tanstack/react-query'
 import { useMemo } from 'react'
 import { useAppwrite } from 'react-appwrite'
 
 /**
  * Retrieves a file URL for download, with no 'Content-Disposition: attachment' header.
+ * @param bucketId The bucket the file belongs to.
+ * @param fileId The unique ID of the file.
+ * @param options Options to pass to `react-query`.
  * @link [Appwrite Documentation](https://appwrite.io/docs/client/storage?sdk=web-default#storageGetFileView)
  */
 export function useFileView(
   bucketId?: string,
   fileId?: string,
+  options?: UseQueryOptions<URL | null, unknown, URL | null, (string | void)[]>
 ) {
   const { storage } = useAppwrite()
   const queryKey = useMemo(() => ['appwrite', 'storage', 'downloads', bucketId, fileId], [bucketId, fileId])
@@ -26,6 +30,8 @@ export function useFileView(
     },
 
     cacheTime: 0,
+
+    ...options,
   })
 
   return queryResult
