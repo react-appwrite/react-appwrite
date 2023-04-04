f859df133df07aa04788643319eada107e4dfcba
sanny-io <sannysherief@gmail.com>
2023-04-03 21:04:30 -0700

    Add service name type

--- src/types.ts
+++ src/types.ts
@@ -1,2 +1,12 @@
 export * from '.'
 export * from './databases/types'
+
+export type AppwriteServiceName =
+  | 'account'
+  | 'avatars'
+  | 'databases'
+  | 'functions'
+  | 'locale'
+  | 'next'
+  | 'storage'
+  | 'teams'
